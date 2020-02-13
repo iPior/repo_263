57cf2067fc0c746ace4e81aace7415f0e371c415
Pior Szaran <piotr.szaran@mail.utoronto.ca>
2020-02-13 09:06:35 -0500

    a2

---
+++ a2/triangle.py
@@ -0,0 +1,22 @@
+'''
+CSC263 Winter 2020
+Problem Set 2 Starter Code
+University of Toronto Mississauga
+'''
+
+# Do NOT add any "import" statements
+# Do NOT use Python dictionaries
+
+def num_triangle_kinds(triangles):
+  '''
+  Pre: triangles is a list of 3-tuples representing triangles
+  Post: return the number of kinds of triangles
+  '''
+  pass # TODO: implement this function
+
+if __name__ == '__main__':
+
+  # some small test cases
+  # Case 1
+  triangles = [(6, 12, 9), (9, 6, 12), (9, 6, 6), (6, 9, 9), (12, 9, 6)]
+  assert 3 == num_triangle_kinds(triangles)
