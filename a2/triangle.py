a271776ad91e552475162f222d796c217a855e3e
Pior Szaran <piotr.szaran@mail.utoronto.ca>
2020-02-27 14:37:05 -0500

    .py fcn

--- a2/triangle.py
+++ a2/triangle.py
@@ -12,7 +12,31 @@ def num_triangle_kinds(triangles):
   Pre: triangles is a list of 3-tuples representing triangles
   Post: return the number of kinds of triangles
   '''
-  pass # TODO: implement this function
+  # TODO: implement this function
+  L = []
+  n = len(triangles) #Avg-Case: O(1)
+  for t in triangles:
+    i = hash(t[0], t[1], t[2], n) #Avg-Case: O(1)
+    if not(i in L): #Avg-Case: O(1)
+      L.append(i) #Avg-Case: O(1)
+  return len(L) #Avg-Case: O(1)
+
+def hash(x, y, z, n):
+    #Multiplication Method for Universal Hashing
+    a = 0.6180339887498949 #as suggested by Donald Knutch
+    m = 2**32 #m big enough so that our hash functions has a slim chance of hashing to the same thing
+
+    #Hash each side individualy
+    x1= m*((x*a)%1)
+    y1= m*((y*a)%1)
+    z1= m*((z*a)%1)
+
+    '''
+    Return the addition of the hashed sides, so that triangles (1,2,3) and (2,2,2)
+    do not have the same hash, but triangles (3,6,12) and (12,3,6) do have the same
+    hash. 
+    '''
+    return x1+y1+z1
 
 if __name__ == '__main__':
 
