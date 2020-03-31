825730197e1cc6ddce2e39b3a76bad6de69916f5
Pior Szaran <piotr.szaran@mail.utoronto.ca>
2020-03-30 21:24:20 -0400

    test case

--- a3/cities.py
+++ a3/cities.py
@@ -104,4 +104,33 @@ if __name__ == '__main__':
 
     assert 1 == can_visit_all_cities(numCities1, dependencies1)
     assert 0 == can_visit_all_cities(numCities2, dependencies2)
-    
+
+    '''
+    [('a','b')
+        ('b','c')
+        ('c','d')
+        ('d','e')
+        ('e','f')
+        ('f','g')
+        ('g','h')
+        ('h','i')
+        ('i','j')
+        ('j','k')
+        ('k','l')
+        ('l','m')
+        ('m','n')
+        ('n','o')
+        ('o','p')
+        ('p','q')
+        ('q','r')
+        ('r','s')
+        ('s','t')
+        ('t','u')
+        ('u','v')
+        ('v','w')
+        ('w','x')
+        ('x','y')
+        ('y','z')
+        ('z','a')]
+        
+    '''
